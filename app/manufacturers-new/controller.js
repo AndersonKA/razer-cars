2ff022a6a627fddb02543a93fb925d333482e89a
Kae Anderson <andersonkaem@gmail.com>
2017-03-21 17:35:36 -0500

    manufacturers new

---
+++ app/manufacturers-new/controller.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+const apiUrl = 'https://razerapi.herokuapp.com/kae/basic/brands';
+
+export default Ember.Controller.extend({
+  formValues: {
+    manufacturer: '',
+    country: '',
+  },
+
+  actions: {
+    save() {
+      fetch(apiUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', accept: 'application/json' },
+        body: JSON.stringify(this.formValues),
+      }).then(r => r.json())
+          .then(() => {
+            this.transitionToRoute('index');
+          });
+    },
+  }
+});
