f6ed6b6f057cba2f5cce7fb44610c0e5fea769e9
Kae Anderson <andersonkaem@gmail.com>
2017-03-24 14:43:21 -0500

    edit button

--- app/router.js
+++ app/router.js
@@ -7,9 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-
   this.route('manufacturer', { path: '/manufacturers' }, function() {
     this.route('new');
+    this.route('edit', { path: '/:brand_id/edit' });
+
     this.route('car', { path: '/:id' }, function() {
       this.route('new');
     });
